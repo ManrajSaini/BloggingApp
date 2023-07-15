61b7a537ef21318d69ceeecf2ea83f7fa4bac5af
Manraj <86542773+ManrajSaini@users.noreply.github.com>
2023-07-15 16:54:07 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // Add CORS headers
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://manrajblog.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
